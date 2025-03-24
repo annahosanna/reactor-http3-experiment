681e6ca12e0092c6eeae39ec5200c1cb01908c0c
annahosanna <annahosanna@gmail.com>
2025-03-24 16:05:22 -0400

    Added http servers and responses

--- src/main/java/example/ReactorHttp3Experiment.java
+++ src/main/java/example/ReactorHttp3Experiment.java
@@ -1,17 +1,26 @@
 package example;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpHeaderValues.TEXT_PLAIN;
-
-import io.netty.handler.ssl.util.SelfSignedCertificate;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import java.io.File;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import reactor.core.publisher.Mono;
+import reactor.netty.NettyOutbound;
+import reactor.netty.http.*;
 import reactor.netty.http.Http11SslContextSpec;
 import reactor.netty.http.Http2SslContextSpec;
 import reactor.netty.http.Http3SslContextSpec;
 import reactor.netty.http.HttpProtocol;
 import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRequest;
+import reactor.netty.http.server.HttpServerResponse;
 
 // This is basically straight from the Reactor examples
+// https://github.com/reactor/reactor-netty/tree/main/reactor-netty-examples/src/main/java/reactor/netty/examples/http
 public final class ReactorHttp3Experiment {
 
   static final boolean SECURE = true;
@@ -20,56 +29,116 @@ public final class ReactorHttp3Experiment {
   static final boolean COMPRESS = true;
 
   public static void main(String[] args) throws Exception {
-    HttpServer server = HttpServer.create()
+    HttpServer serverV11 = HttpServer.create()
+      .port(80)
+      .wiretap(WIRETAP)
+      .compress(false)
+      .route(routes ->
+        routes.route(r -> true, ReactorHttp3Experiment::okResponseV11)
+      );
+
+    HttpServer serverV2 = HttpServer.create()
       .port(PORT)
       .wiretap(WIRETAP)
       .compress(COMPRESS)
-      .route(r ->
-        r.post("/echo", (req, res) ->
-          res.header(CONTENT_TYPE, TEXT_PLAIN).send(req.receive().retain())
-        )
+      .route(routes ->
+        routes.route(r -> true, ReactorHttp3Experiment::okResponseV2)
       );
 
-    if (SECURE) {
-      SelfSignedCertificate ssc = new SelfSignedCertificate();
-      if (HTTP2) {
-        server = server.secure(spec ->
-          spec.sslContext(
-            Http2SslContextSpec.forServer(ssc.certificate(), ssc.privateKey())
-          )
-        );
-      } else if (HTTP3) {
-        server = server.secure(spec ->
-          spec.sslContext(
-            Http3SslContextSpec.forServer(ssc.key(), null, ssc.cert())
-          )
-        );
-      } else {
-        server = server.secure(spec ->
-          spec.sslContext(
-            Http11SslContextSpec.forServer(ssc.certificate(), ssc.privateKey())
-          )
-        );
-      }
-    }
+    serverV2 = serverV2.secure(spec ->
+      spec.sslContext(
+        Http2SslContextSpec.forServer(
+          new File("certs.pem"),
+          new File("key.pem")
+        )
+      )
+    );
+    serverV2 = serverV2.protocol(HttpProtocol.H2);
 
-    if (HTTP2) {
-      server = server.protocol(HttpProtocol.H2);
-    }
+    HttpServer serverV3 = HttpServer.create()
+      .port(PORT)
+      .wiretap(WIRETAP)
+      .compress(COMPRESS)
+      .route(routes ->
+        routes.route(r -> true, ReactorHttp3Experiment::okResponseV3)
+      );
 
-    if (HTTP3) {
-      server = server
-        .protocol(HttpProtocol.HTTP3)
-        .http3Settings(spec ->
-          spec
-            .idleTimeout(Duration.ofSeconds(5))
-            .maxData(10000000)
-            .maxStreamDataBidirectionalLocal(1000000)
-            .maxStreamDataBidirectionalRemote(1000000)
-            .maxStreamsBidirectional(100)
-        );
-    }
+    serverV3 = serverV3.secure(spec ->
+      spec.sslContext(
+        Http3SslContextSpec.forServer(
+          new File("key.pem"),
+          null,
+          new File("certs.pem")
+        )
+      )
+    );
 
-    server.bindNow().onDispose().block();
+    // QUIC does not know what the idletimeout etc should be, since it doesn't know HTTP3 will be the upper layer. Those need to be adjusted here
+    serverV3 = serverV3
+      .protocol(HttpProtocol.HTTP3)
+      .http3Settings(spec ->
+        spec
+          .idleTimeout(Duration.ofSeconds(5))
+          .maxData(10000000)
+          .maxStreamDataBidirectionalLocal(1000000)
+          .maxStreamDataBidirectionalRemote(1000000)
+          .maxStreamsBidirectional(100)
+      );
+
+    serverV11.bindNow().onDispose().block();
+    serverV2.bindNow().onDispose().block();
+    serverV3.bindNow().onDispose().block();
+  }
+
+  private static NettyOutbound okResponseV11(
+    HttpServerRequest request,
+    HttpServerResponse response
+  ) {
+    String responseText = new String(
+      "<html><a href=\"/fortune\">fortune</a></html>"
+    );
+    response.status(426);
+    response.header("Content-Type", "text/html");
+    response.header("Content-Length", Integer.toString(responseText.length()));
+    response.header("Upgrade-Insecure-Requests", "1");
+    Mono<String> responseContent;
+    responseContent = Mono.just(responseText);
+    return response.sendString(responseContent);
+  }
+
+  private static NettyOutbound okResponseV2(
+    HttpServerRequest request,
+    HttpServerResponse response
+  ) {
+    String responseText = new String(
+      "<html><a href=\"/fortune\">fortune</a></html>"
+    );
+    response.status(200);
+    response.header("Content-Type", "text/html");
+    response.header("Content-Length", Integer.toString(responseText.length()));
+    response.header(
+      "Alt-Svc",
+      "h3=\":443\"; ma=86400, h3-29=\":443\"; ma=86400, h3-Q050=\":443\"; ma=86400, h3-Q046=\":443\"; ma=86400, h3-Q043=\":443\"; ma=86400, quic=\":443\"; ma=86400; v=\"43,46\""
+    );
+    response.header("Application-Protocol", "h3,quic,h2,http/1.1");
+    Mono<String> responseContent;
+    responseContent = Mono.just(responseText);
+    return response.sendString(responseContent);
+  }
+
+  private static NettyOutbound okResponseV3(
+    HttpServerRequest request,
+    HttpServerResponse response
+  ) {
+    String responseText = new String(
+      "<html><a href=\"/fortune\">fortune</a></html>"
+    );
+    response.status(200);
+    response.header("Content-Type", "text/html");
+    response.header("Content-Length", Integer.toString(responseText.length()));
+    response.header("Alt-Svc", "h3=\":443\"; ma=86400");
+    Mono<String> responseContent;
+    responseContent = Mono.just(responseText);
+    return response.sendString(responseContent);
   }
 }
