bedb6505b3b092d246f04d0591151b6da62c0e8a
annahosanna <annahosanna@gmail.com>
2025-03-31 20:49:24 -0400

    Rewrote for decoding

--- src/main/java/example/ServeHttp2.java
+++ src/main/java/example/ServeHttp2.java
@@ -39,6 +39,7 @@ public class ServeHttp2 {
     // ServeCommon.fixContentType(request);
     // System.out.println("Adding a fortune");
     Mono<String> monoString = ServeCommon.getFormData(request);
+    monoString.subscribe(result -> { System.out.println("Lambda result: " + result);})
     return response.sendString(monoString);
   }
 
