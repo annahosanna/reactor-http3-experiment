9eae53db109f2fa159897c2349d56e74ca6b9b70
annahosanna <annahosanna@gmail.com>
2025-04-13 20:29:46 -0400

    Fix static hostname

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+hostname=$(hostname)
+time=$(date +%s)
 # Test rapid connections
 for i in {1..10000}
 do
-time=$(date +%s)
-curl --cacert ./certs.pem http://gks-macbook-air.local/fortune -d "key${i}=$time" -H "Content-Type:application/x-www-form-urlencoded" &
+curl --cacert ./certs.pem http://${hostname}/fortune -d "key${i}=$time" -H "Content-Type:application/x-www-form-urlencoded" &
 done
