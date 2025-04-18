9631708c0194f15b066841a4078907898ea8bb9f
annahosanna <annahosanna@gmail.com>
2025-04-18 02:05:07 -0400

    Make MVP example

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
 hostname=$(hostname)
 time1=$(date +%s)
 echo "Start time: ${time1}"
 # Test rapid connections
 for i in {1..10000}
 do
-  sleep 0.00001
-  curl --cacert ./certs.pem http://${hostname}/fortune -d "key${i}=${time1}" -H "Content-Type:application/x-www-form-urlencoded" &
+  sleep 0.000001
+  curl --cacert ./certs.pem http://${hostname}/fortune -d "key${i}=${time1}" -H "Content-Type:application/x-www-form-urlencoded" -o /dev/null > /dev/null 2>&1 &
 done
 time2=$(date +%s)
 echo "End time (estimated): ${time2}"
