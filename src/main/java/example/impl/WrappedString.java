b2560e37225fc8725b637bf3a5c8f9d9680a4090
annahosanna <annahosanna@gmail.com>
2025-06-28 01:17:24 -0400

    Add a helper class

---
+++ src/main/java/example/impl/WrappedString.java
@@ -0,0 +1,16 @@
+package example.impl;
+
+public class WrappedString {
+
+  private String wrappedString = null;
+
+  public WrappedString() {}
+
+  public String getWrappedString() {
+    return wrappedString;
+  }
+
+  public void setWrappedString(String wrappedString) {
+    this.wrappedString = wrappedString;
+  }
+}
